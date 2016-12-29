07dbb82fb247067e2a793687d297ba598c1a8107
Alex Gillespie <alexmgillespie@gmail.com>
2016-12-29 14:53:07 +0000

    Added profiles controller, new view and link on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  devise_for :users, controllers: { registrations: 'users/registrations' }
-  resources :users do
+  root to: 'pages#home' #Home page
+  devise_for :users, controllers: { registrations: 'users/registrations' } #User registration - Devise
+  resources :users do #Nested resource for profile pages
     resource :profile
   end
-  get 'about', to: 'pages#about'
-  resources :contacts, only: :create
-  get 'contact-us', to: 'contacts#new', as: 'new_contact'
+  get 'about', to: 'pages#about' #About page
+  resources :contacts, only: :create #Contacts page
+  get 'contact-us', to: 'contacts#new', as: 'new_contact' #Re-route contact to usable URL
 end
